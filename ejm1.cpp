53acc755534f7c30bd41a12a9b571be3b3309450
sergiio12345 <sergio42rafa@gmail.com>
2025-05-19 21:31:17 -0500

    Update ejm1.cpp

--- ejm1.cpp
+++ ejm1.cpp
@@ -20,6 +20,37 @@ void imprimirisr(int arr[]){
     }
 }
 
+void merge(int arr1[], int n1, int arr2[], int n2, int arr[]){
+    int i = 0, j = 0 , k = 0;
+
+    while(i < n1 && j < n2){
+         if(arr1[i] < arr2[j]){
+             arr[k] = arr1[i];
+             i++;
+         }else{
+             arr[k] = arr2[j];
+             j++;
+         }
+         k++;
+    }
+    while(i < n1){
+        arr[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        arr[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+
+void imprimirArreglo(int a[], int n){
+    for(int i = 0; i < n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
 int main(){
     int arreglo[]={2,1,5,3,4};
 
